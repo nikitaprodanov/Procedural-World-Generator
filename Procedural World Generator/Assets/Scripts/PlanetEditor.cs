af9207e70b19d1c56bbcf73e059633569746502a
uchihanaruto9371 <uchihanaruto9371@gmail.com>
2020-05-10 16:16:34 +0300

    Planet settings are available from editor

---
+++ Procedural World Generator/Assets/Scripts/PlanetEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+[CustomEditor(typeof(Planet))]
+public class PlanetEditor : Editor
+{
+    Planet planet;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawSettingsEditor(planet.shapeSettings);
+        DrawSettingsEditor(planet.colorSettings);
+    }
+
+    void DrawSettingsEditor(Object settings)
+    {
+        Editor editor = CreateEditor(settings);
+        editor.OnInspectorGUI();
+    }
+
+    private void OnEnable()
+    {
+        planet = (Planet)target;
+    }
+}
